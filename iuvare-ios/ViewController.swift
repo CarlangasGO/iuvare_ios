826a444c273943b7d502bb5356060333ed850c04
schultzRr <ZFja5Hdb>
2016-10-12 18:53:37 -0500

    Added loading overlay and internet connection and iframe loading error alerts

--- iuvare-ios/ViewController.swift
+++ iuvare-ios/ViewController.swift
@@ -11,20 +11,67 @@ import AVFoundation
 
 class ViewController : UIViewController, UIWebViewDelegate {
     @IBOutlet weak var webView: UIWebView!
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let url = URL (string: "http://socios.iuvare.mx")
-        let requestObj = URLRequest(url: url!)
-        webView.delegate = self
-        webView.loadRequest(requestObj)
+        
+        loadWebView()
         
         do {
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
         }
         catch {
             // report for an error
+        }
+    }
+    
+    func loadWebView() {
+        LoadingIndicatorView.show("Cargando")
+        
+        if Reachability.isConnectedToNetwork() == true {
+            print("Internet connection OK")
+            
+            // Do any additional setup after loading the view, typically from a nib.
+            let url = URL (string: "http://socios.iuvare.mx")
+            let requestObj = URLRequest(url: url!)
+            webView.delegate = self
+            webView.loadRequest(requestObj)
+        } else {
+            print("Internet connection FAILED")
+            
+            let alert = UIAlertController(title: "No hay conexión a internet", message: "Conéctate a una red Wi-Fi o habilita tus datos celulares.", preferredStyle: UIAlertControllerStyle.alert)
+            
+            let settingsAction = UIAlertAction(title: "Ir a ajustes", style: .default) { (_) -> Void in
+                
+            }
+            alert.addAction(settingsAction)
+            
+            let retryAction = UIAlertAction(title: "Volver a intentar", style: UIAlertActionStyle.default, handler: { (action: UIAlertAction!) in
+                self.loadWebView()
+            })
+            alert.addAction(retryAction)
+            
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
+            
+        }
+    }
+    
+    func webViewDidFinishLoad(_ webView : UIWebView) {
+        // dismiss the loading indicator view once webView has loaded
+        LoadingIndicatorView.hide()
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        let alert = UIAlertController(title: "Lo sentimos", message: "Ocurrió un error al cargar el contenido de la app.", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addAction(UIAlertAction(title: "Volver a intentar", style: UIAlertActionStyle.default, handler: { (action: UIAlertAction!) in
+            self.loadWebView()
+        }))
+        
+        DispatchQueue.main.async {
+            self.present(alert, animated: true, completion: nil)
         }
     }
     
